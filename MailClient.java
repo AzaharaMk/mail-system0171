08ccb40ac88392b9d8279cfd8e02f3d662eba74c
AzaharaMk <azahara.mk@gmail.com>
2016-11-25 14:18:45 +0100

    0550.a ultimos mensajes

--- MailClient.java
+++ MailClient.java
@@ -17,6 +17,8 @@ public class MailClient
     private String autoSubject;
    //Mensaje de la autorespuesta
     private String autoMessage;
+    
+    private MailItem ultimoMensaje;
 
     /**
      * Create a mail client run by user and attached to the given server.
@@ -35,11 +37,28 @@ public class MailClient
      */
     public MailItem getNextMailItem()
     {
+        //Recibimos  algo  del servidor
         MailItem item = server.getNextMailItem(user);
+        if (item != null)
+        {
+            ultimoMensaje = item;
+        }
+        //Si lo que recibimos es un email y la  respuesta automática está activa...
         if (answerMode && item != null)
         { 
-            sendMailItem(item.getFrom(), autoMessage, autoSubject);
+            //Enviamos un correo de respuesta  automáticamente
+            //creamos el email
+            MailItem email =  new MailItem (user, item.getFrom(),
+                                            autoSubject, autoMessage);
+            //Enviamos el email
+            server.post(email);
+            
+            /**otra forma de enviar el mensaje:
+             *  sendMailItem(item.getFrom(), autoMessage, autoSubject);
+             */
         }
+        ultimoMensaje = item;
+        //Devolvemos lo recibido por el servidor
         return item;
     }
 
@@ -49,7 +68,13 @@ public class MailClient
      */
     public void printNextMailItem()
     {
-        MailItem item = server.getNextMailItem(user);
+        MailItem item = getNextMailItem();
+        
+        if (item != null)
+        {
+            ultimoMensaje = item;
+        }
+        
         if(item == null) {
             System.out.println("No new mail.");
         }
@@ -70,19 +95,43 @@ public class MailClient
         server.post(item);
     }
     
-    public void setAutomaticAnswer(boolean answer)
+    //Habilita o deshabilita la respuesta automática
+    public void setAutomaticAnswer()
     {
-        answerMode = answer;
+        if (answerMode)
+        {
+            answerMode = false;
+        }
+        else
+        {
+            answerMode = true;           
+        }
     }
     
-    public void setAutoContent (String subject, String message)
+    /**
+     * Permite configurar el texto del asunto y del mensaje 
+     * de la respuesta automatica
+     */
+    public void configurarRespuestaAutomatica (String subjectAuto, String messageAuto)
     {
-        autoSubject = subject;
-        autoMessage = message;
+        this.autoSubject = subjectAuto;
+        this.autoMessage = messageAuto;
     }
     
     public void numberMail()
     {
         System.out.println("emails almacenados: " +server.howManyMailItems(user));
+    }
+    
+    public void printUltimoMensaje()
+    {
+        if (ultimoMensaje == null)
+        {
+            System.out.println ("No ha recibido mensajes");
+        }
+        else
+        {
+            ultimoMensaje.print();
+        }
     }
 }
